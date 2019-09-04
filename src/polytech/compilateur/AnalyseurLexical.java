97b402121dcb6bcd042d81e5d40178016d0503ef
Niko <nicolas.bourneuf.polytech@gmail.com>
2019-09-04 12:23:19 +0200

    analyseLexiquePresqueFinie

--- src/polytech/compilateur/AnalyseurLexical.java
+++ src/polytech/compilateur/AnalyseurLexical.java
@@ -69,33 +69,91 @@ public class AnalyseurLexical {
 
 		String line; 
 		int lineIndex = 0;
+		int columnIndex = 0;
+		boolean blockComment = false;
 		while ((line = reader.readLine()) != null) {
 			//ATTENTION :
 			//PASSER LES COMMENTAIRES (// ou /**/)
 			//NOTER LES FINS DE LIGNES ET LA FIN DE FICHIER
-
-			//ASCII NUMBERS : 48 TO 57
-			//ASCII MINUS  : 97 - 122
-			//ASCII MAJUSCULE : 65 - 90 
-			//UNDERSCORE ET TIRETS DANS WHILE VARIABLE
 			//ESPACE : 32
 			
-			for(int columnIndex = 0; columnIndex < line.length(); columnIndex++) {
+			for(columnIndex = 0; columnIndex < line.length(); columnIndex++) {
 				
 				char actualChar = line.charAt(columnIndex);
 				int asciiChar = (int) actualChar;
 				
-				if(asciiChar >= 48 && asciiChar <= 57) {
+				//Si on est face à un commentaire de ligne
+				//ASCII "/" : 47
+				if(asciiChar == 47 && line.codePointAt(columnIndex+1) == 47) {
+					//On passe à la ligne suivante
+					columnIndex = line.length() - 1;
+				}
+				//on est face à un commentaire de bloque
+				//ASCII "/*" : 47 et 42
+				else if((asciiChar == 47 && line.codePointAt(columnIndex+1) == 42) || blockComment) {
+					
+					blockComment = true;
 					
 					int endIndex = columnIndex+1;
-					while(line.codePointAt(endIndex) >= 48 && line.codePointAt(endIndex) <= 57){
+					//Tant qu'il n'y a pas de fin de commentaire de block on avance dans le fichier sans rien faire
+					//ASCII "*/" : 42 et 47
+					while(endIndex+1 < line.length() && line.codePointAt(endIndex) != 42 && line.codePointAt(endIndex+1) != 47){
+						endIndex++;
+					}
+					
+					if(line.codePointAt(endIndex) == 42 && line.codePointAt(endIndex+1) == 47) {
+						blockComment = false;
+					}
+					
+					//On se replace au bon endroit dans le fichier
+					columnIndex = endIndex;
+				}
+				//on est face à un chiffre
+				//ASCII NUMBERS : 48 TO 57
+				else if(asciiChar >= 48 && asciiChar <= 57) {
+					
+					int endIndex = columnIndex+1;
+					//TANT QU'IL Y A DES CHIFFRES ET QU'ON EST PAS EN FIN DE LIGNE
+					while(endIndex < line.length() && line.codePointAt(endIndex) >= 48 && line.codePointAt(endIndex) <= 57){
 						endIndex++;
 					}
 					
 					int value = Integer.parseInt(line.substring(columnIndex, endIndex));
+					//On cré un token a avec le nombre récupéré
+					this.tokenList.add(new Token("tok_constant", value, lineIndex, columnIndex));
 					
-					this.tokenList.add(new Token("tok_constante", value, lineIndex, columnIndex));
-						
+					//On se replace au bon endroit dans le fichier
+					columnIndex = endIndex;
+				}
+				//on est face à une lettre ou un underscore : création d'une variable
+				//ASCII MINUS  : 97 - 122
+				//ASCII MAJUSCULE : 65 - 90 
+				//UNDERSCORE : 95
+				else if((asciiChar >= 97 && asciiChar <= 122) || (asciiChar >= 65 && asciiChar <= 90) || asciiChar == 95) {
+					
+					int endIndex = columnIndex+1;
+					//TANT QU'IL Y A DES lettres ou un underscore ET QU'ON EST PAS EN FIN DE LIGNE
+					while(endIndex < line.length() && ((line.codePointAt(endIndex) >= 97 && line.codePointAt(endIndex) <= 122) || (line.codePointAt(endIndex) >= 65 && line.codePointAt(endIndex) <= 90) || line.codePointAt(endIndex) == 95)){
+						endIndex++;
+					}
+					
+					//On récupère le texte
+					String name = line.substring(columnIndex, endIndex);
+					
+					//Si c'est un mot clé pour une instruction on cré un token qui est lié à ce mot clé
+					if(this.keywords.containsKey(name)) {
+						this.tokenList.add(new Token(this.keywords.get(name), name, lineIndex, columnIndex));
+					}
+					//Sinon c'est une variable donc on cré un token identificateur
+					
+					//On cré un token a avec le nombre récupéré
+					this.tokenList.add(new Token("tok_identifier", name, lineIndex, columnIndex));
+					
+					//On se replace au bon endroit dans le fichier
+					columnIndex = endIndex;
+				}
+				else {
+					
 				}
 				
 			}
@@ -103,6 +161,8 @@ public class AnalyseurLexical {
 			
 			lineIndex += 1;
 		}
+		
+		this.tokenList.add(new Token("tok_end_of_file", lineIndex, columnIndex));
 
 		reader.close();
 
